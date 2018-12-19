b5ebbe58c21d1080577da43f0d6da622dcecec51
dedeu99 <andre.calixto.martins@gmail.com>
2018-12-19 08:35:07 +0000

    asd

--- application/controllers/orders.php
+++ application/controllers/orders.php
@@ -2,7 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Orders extends CI_Controller {
+		public function __construct()
+		{
+			parent::__construct();
+	
+			$this->load->library('session');
 
+			
+		}
 		public function index()
 		{
 			$this->load->view('welcome_message');
