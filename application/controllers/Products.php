b98937e32619dd91de2d1485d35c7d9889011ecb
dedeu99 <andre.calixto.martins@gmail.com>
2018-12-19 05:37:14 +0000

    asd

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -10,7 +10,21 @@
 			$this->load->model('product');
 		}
 		public function index(){
-			echo 'sucess';
+			if(!function_exists('apache_get_modules') ){ phpinfo(); exit; }
+ $res = 'Module Unavailable';
+ if(in_array('mod_rewrite',apache_get_modules())) 
+ $res = 'Module Available';
+
+			echo (<html>
+			<head>
+			<title>A mod_rewrite availability check !</title></head>
+			<body>
+			<p><?php echo apache_get_version(),"</p><p>mod_rewrite $res"; ?></p>
+			</body>
+			</html>);
+
+
+
 		}
 		public function getProductsByCategory($cat_id){
 			
