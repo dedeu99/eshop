9888181595ec2742fdfb8c3448eba38a014d0d8f
dedeu99 <andre.calixto.martins@gmail.com>
2018-12-19 10:21:51 +0000

    asd

---
+++ application/controllers/Orders.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Orders extends CI_Controller {
+		public function __construct()
+		{
+			parent::__construct();
+			$this->load->model('order');
+			$this->load->library('session');
+
+			
+		}
+		public function index()
+		{
+			$this->load->view('welcome_message');
+		}
+
+		public function orderProduct($productid=-1,$quantity=-1)
+		{
+			if(intval($quantity)<=0)
+				return;
+			echo "<script> alert('PHP: ',".$quantity.");</script>"
+			if(!isset($_SESSION['cart']))
+				$_SESSION['cart']=[];
+
+			if(isset($_SESSION['cart'][$productid]))
+				$_SESSION['cart'][$productid]+=intval($quantity);
+			else{
+				$_SESSION['cart'][$productid]= intval($quantity);
+			}
+		}
+	}
+?>
