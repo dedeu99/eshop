9a4054454c2f5fba308b15afa4b483371074adaa
dedeu99 <andre.calixto.martins@gmail.com>
2018-12-19 05:03:07 +0000

    asd

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'index';
-$route['product/(:num)'] = /*base_url()+*/'index.php/products/getProductsByCategory/';
+$route['product/(:num)'] = /*base_url()+*/'index.php/products/getProductsByCategory/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
