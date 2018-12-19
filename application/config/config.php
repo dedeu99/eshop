709572423ef2aad335bceeb03e6242356d800fb0
dedeu99 <andre.calixto.martins@gmail.com>
2018-12-19 05:30:45 +0000

    asd

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '/~a62362/LAB12';
+$config['base_url'] = '/~a62362/LAB12/index.php/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = '/~a62362/LAB12';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
