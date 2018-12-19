21b44af0088202c3725aef2f833dd7f54312a49e
dedeu99 <andre.calixto.martins@gmail.com>
2018-12-19 05:35:25 +0000

    asd

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,7 +52,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'index';
 $route['product'] =  function ()
 {
-	echo "I GOT CALLED";
+	<?php
+ if(!function_exists('apache_get_modules') ){ phpinfo(); exit; }
+ $res = 'Module Unavailable';
+ if(in_array('mod_rewrite',apache_get_modules())) 
+ $res = 'Module Available';
+?>
+<html>
+<head>
+<title>A mod_rewrite availability check !</title></head>
+<body>
+<p><?php echo apache_get_version(),"</p><p>mod_rewrite $res"; ?></p>
+</body>
+</html>
         return '';
 };
 
