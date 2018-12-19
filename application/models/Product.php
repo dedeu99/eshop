61e40eda203cbbdf33e4a167fe49741d31af97ff
dedeu99 <andre.calixto.martins@gmail.com>
2018-12-19 03:34:01 +0000

    asd

---
+++ application/models/Product.php
@@ -0,0 +1,22 @@
+<?php
+class Blog_model extends CI_Model {
+	public function __construct()
+	{
+		$this->load->database();
+	}
+	public function createOrder()
+	{
+		
+	}
+	public function getOrders($costumer_id)
+	{
+		
+	}
+	public function getOrder($order_id)
+	{
+		
+	}
+
+
+}
+?>
